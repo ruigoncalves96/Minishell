d649ab46e12d4a64742e20ff4cf346eea260e7f2
Rui Gonçalves <ruigoncalves@Ruis-MacBook-Pro.local>
2025-01-08 15:49:20 +0000

    define redefine

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: randrade <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 15:05:53 by randrade          #+#    #+#             */
-/*   Updated: 2025/01/08 15:31:22 by randrade         ###   ########.fr       */
+/*   Updated: 2025/01/08 15:49:11 by randrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,15 @@ static void	ft_print_linked_list(t_list *list)
 			ft_printf("      = type = COMMAND\n");
 		else if (temp->type == OPERATOR)
 			ft_printf("      = type = OPERATOR\n");
-		if (temp->subtype == WORD)
+		if (temp->subtype == T_WORD)
 			ft_printf("      = subtype = WORD\n");
-		else if (temp->subtype == QUOTE)
+		else if (temp->subtype == T_QUOTE)
 			ft_printf("      = subtype = QUOTE\n");
-		else if (temp->subtype == DOLLAR)
+		else if (temp->subtype == T_DOLLAR)
 			ft_printf("      = subtype = DOLLAR\n");
-		else if (temp->subtype == SPACE)
+		else if (temp->subtype == T_SPACE)
 			ft_printf("      = subtype = SPACE\n");
-		else if (temp->subtype == PIPE)
+		else if (temp->subtype == T_PIPE)
 			ft_printf("      = subtype = PIPE\n");
 		else if (temp->subtype == REDIRECT)
 			ft_printf("      = subtype = REDIRECT\n");
