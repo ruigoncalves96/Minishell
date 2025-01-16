6d04f29a07562743ad4e4a238bdf25882b59ec2e
Hugo Pinto <hp052635@gmail.com>
2025-01-16 20:55:33 +0000

    reddirect done

--- src/executer/redirections.c
+++ src/executer/redirections.c
@@ -1,19 +1,12 @@
 #include "../../includes/minishell.h"
 
-/*
-    Ideia da funcao, percorrer uma lista de comandos e printar coisas com base no tipo de dado
-    
-    [] Percorrer a linha e abrir todos os fds que forem precisos
-    [X] Executar um comando se for tipo Comando
-*/
-//[]Funcao que vai criar uma list t_redirect
 
 static int open_redirect(t_token *token)
 {
     if (!token || !token->red || !token->red->filename[0])
         return 1;
     //[X] abrir o arquivo
-    printf("Redirect filename: %s\n", token->red->filename[0]);
+   // printf("Redirect filename: %s\n", token->red->filename[0]);
     if(token->red->type == OUT)
         token->red->fd = open(token->red->filename[0], O_WRONLY | O_CREAT | O_TRUNC, 0644);
     else if(token->red->type == A_OUT)
@@ -43,37 +36,45 @@ static void loop_and_open_fd(t_token *token)
     
 }
 
-static void handle_redirections(t_token *token,int *backup_fd)
+static void handle_redirections(t_token *token, int *backup_fd)
 {
+    
     if (!token->next || token->next->subtype != T_REDIRECT)
         return;
 
-    
-      printf("Entrei\n");
     backup_fd[0] = dup(STDIN_FILENO);
     backup_fd[1] = dup(STDOUT_FILENO);
 
-    while(token->next->subtype == T_REDIRECT)
+    while (token->next && token->next->subtype == T_REDIRECT)
     {
-        if(token->next->red->type == OUT || token->next->red->type == A_OUT)
+        if (token->next->red->type == OUT || token->next->red->type == A_OUT)
         {
-            
-            if(dup2(token->next->red->fd,STDOUT_FILENO) == -1)
+            if (dup2(token->next->red->fd, STDOUT_FILENO) == -1)
             {
                 perror("dup2");
-                close(token->next->red->fd);
+                return;
             }
         }
+        else if (token->next->red->type == IN)
+        {
+            if (dup2(token->next->red->fd, STDIN_FILENO) == -1)
+            {
+                perror("dup2");
+                return;
+            }
+        }
+        close(token->next->red->fd);  // Fechar FD após o dup2
+        token->next->red->fd = -1;    // Marcar como fechado
         token = token->next;
     }
-
 }
-static void close_stuff(int *backup_fd)
+static void close_stuff( int *backup_fd)
 {
-            dup2(backup_fd[0], STDIN_FILENO);   
-            dup2(backup_fd[1], STDOUT_FILENO);
-            close(backup_fd[0]);
-            close(backup_fd[1]);
+
+    dup2(backup_fd[0], STDIN_FILENO);
+    dup2(backup_fd[1], STDOUT_FILENO);
+    close(backup_fd[0]);
+    close(backup_fd[1]);
 }
 //[X]Primeiro loop para abrir as coisas
 //[] Loop para verificar comandos
@@ -85,13 +86,22 @@ void loop_executer(t_token *token,t_env *env,t_prompt_info prompt_info)
     {
         if(token->type == COMMAND)
         {
-            if(validate_command_path(*token->token,env) == 0 || execute_builtin(token,prompt_info) == 0)//Comando valido 
+            if(token->next && token->next->type == T_PIPE)
             {
-                    
-                    handle_redirections(token,original_fd);
-                    executer_manager(token->token,env); 
-                    close_stuff(original_fd);
-            }                  
+                printf("vem ai o monstro aiai\n");
+            }else
+            {
+                handle_redirections(token,original_fd);
+                if(is_builtin(*token->token))
+                {
+                    execute_builtin(token,prompt_info);
+                }
+                else if(validate_command_path(*token->token,env) == 0)
+                {
+                    executer_manager(token->token,env);
+                }
+                close_stuff(original_fd);                             
+            }
         }
         token = token->next;
     }
@@ -126,4 +136,4 @@ int executer_manager( char **str,t_env *env)
             ft_free_double_array(env_array);
       
     return 0;
-    }
+}
